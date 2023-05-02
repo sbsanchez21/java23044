78f7096f22331110b52febb2be3069f6305a83e0
santos <santos.sanchez@bue.edu.ar>
2023-05-02 16:35:05 -0300

    js clase3

---
+++ 04_javascript/Clase3/clase/js/javascript.js
@@ -0,0 +1,31 @@
+// alert("hola js")
+let nombreMascota="Pupy"
+// alert(nombreMascota)
+
+//es mejor utilizar let
+var cantClientes=15
+//se sugiere usar let se destruye cuando se sale del alcance
+let cantAlumnos=10
+
+// aplicación variables
+let usuario=""
+let pass=""
+// usuario=prompt("Ingrese su usuario")
+// pass=prompt("Ingrese su password")
+console.log("El usuario y pass ingresados son: " + usuario + " " + pass)
+
+//variables numéricas
+let x=5
+let y=3
+
+let nombre="Juan"
+let apellido="Suarez"
+
+let resultado=x+y
+console.log("suma de x + y = " + resultado )
+
+resultado=x*y
+console.log("resultado de x * y = " + resultado )
+
+console.log("Concatenar: " + apellido + ", " + nombre)
+console.log(`Bienvenido a nuestra página ${apellido}, ${nombre}`)
