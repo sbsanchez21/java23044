08626ca11b1fba687f187ddf6b22575a671e8201
santos <santos.sanchez@bue.edu.ar>
2023-05-09 16:34:55 -0300

    js clase5

---
+++ 04_javascript/Clase5/clase/js/funciones.js
@@ -0,0 +1,34 @@
+let a=3
+let b=4
+console.log(`a+b= ${a+b}`)
+
+let nombre="Juan"
+let apellido="Perez"
+
+// validarUsuario()
+
+function validarUsuario() {
+    console.log("ingresó a la función")
+    let usu=prompt("usuario")
+    let pass=prompt("password")
+
+    if (usu=="psuarez@gmail.com" && pass=="1234") {
+        console.log("válido")
+    } else {
+        console.log("Inválido")
+    }
+}
+
+// validarUsuario()
+
+function mostrarFortaleza() {
+    console.log("ingreso a funcion mostrarFortaleza")
+    console.log(document.getElementById("pass").value)
+    let pass=document.getElementById("pass").value
+
+    if (pass.length<=4) {
+        console.log("fortaleza baja")
+    } else {
+        console.log("fortaleza alta")
+    }
+}
